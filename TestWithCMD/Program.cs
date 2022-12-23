3b8323c0974a70797544232d4c9895eafc37606c
Yusif <yusifyp@code.edu.az>
2022-12-23 18:04:21 +0400

    master

---
+++ TestWithCMD/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestWithCMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Test With CMD");
+
+        }
+    }
+}
