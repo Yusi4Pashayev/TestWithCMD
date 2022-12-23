0d9f84d93bbb5bdf1c4f334f1965d7205a5cb08a
Yusif <yusifyp@code.edu.az>
2022-12-23 18:07:19 +0400

    dev

--- TestWithCMD/Program.cs
+++ TestWithCMD/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Test With CMD");
+            Console.WriteLine("we create new branch dev");
 
         }
     }
